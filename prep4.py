93178d0f0ac3f26cbf3192dddd3e219576493104
Elliott Cepin <vaylndev@gmail.com>
2022-08-29 19:09:34 -0700

    updated for autograder

--- prep4.py
+++ prep4.py
@@ -1,9 +1,9 @@
 import ans
 
 to_inches = lambda feet: "Inches: " + str(round(feet * 12, 3))
-to_meters = lambda feet: "Meters: " + str(round(feet * .3048, 3))
-to_rods   = lambda feet: "Rods: "   + str(round(feet * 0.061, 3))
+to_meters = lambda feet: "Meter: " + str(round(feet * .3048, 3))
+to_rods   = lambda feet: "Rods: "   + str(round(feet * 0.061, 1))
 
-
-feet = ans.ans("Number of feet:", get_int=True, formatting_for_the_stupid_autograder="", newline=False)
-print("", to_inches(feet), to_meters(feet), to_rods(feet), sep="\n")
+if __name__ == '__main__':
+    feet = ans.ans("Number of feet:", get_int=True, formatting_for_the_stupid_autograder="", newline=False)
+    print("", to_inches(feet), to_meters(feet), to_rods(feet), sep="\n")
