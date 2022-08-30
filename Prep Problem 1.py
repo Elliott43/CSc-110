1339108c022eca964cff58eb661795aa13afcafd
Elliott Cepin <vaylndev@gmail.com>
2022-08-29 18:44:55 -0700

    Cool Stuff done

--- Prep Problem 1.py
+++
@@ -1,7 +0,0 @@
-###
-### Author: Your name here
-### Course: CSc 110
-### Description: Prints "Hi there!"
-###
-
-print("Hi there!")
--- ans.py
+++ ans.py
@@ -1,10 +1,12 @@
 # This function is my pride and joy 
-def ans(message="", options=(), get_int=False):
+def ans(message="", options=(), get_int=False, formatting_for_the_stupid_autograder=">> ", newline=True):
     assert not (options and get_int), "two conflicting parameters, 'options' and 'get_int', were assigned values"
     output = None
 
     if message:
-        print(message)
+        print(message, end="")
+
+    if newline:
         print()
 
     if options:
@@ -18,7 +20,7 @@ def ans(message="", options=(), get_int=False):
         not_in_range = ""
 
         try:
-            output = int(input(">> "))
+            output = int(ans("", (), False, formatting_for_the_stupid_autograder))
 
             if isinstance(get_int, tuple):
                 if not ((output >= get_int[0]) and (output <= get_int[-1])):
@@ -32,7 +34,7 @@ def ans(message="", options=(), get_int=False):
             output = ans(get_int=get_int)
 
     else:
-        output = input(">> ")
+        output = input(formatting_for_the_stupid_autograder)
 
     # I am aware that in a majority of the places where there is an "output = thing" I could have put a "return thing"
     # That was aesthetically (and also maybe programmatically) unpleasing
